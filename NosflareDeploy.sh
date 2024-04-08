1dda6dae9493ff2f1ce0a414c46b98b7f97c3c37
PastaGringo <pastagringo@protonmail.com>
2024-04-08 17:44:30 +0200

    v1.10.7 OK

--- NosflareDeploy.sh
+++ NosflareDeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version_check="1.9.7"
+version_check="1.10.7"
 #
 # # MANDATORY BEGIN ###########################################################################################
 # Please update variables below before running the script
